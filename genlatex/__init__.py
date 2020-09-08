be403cdadc8b02d36712d298daac90f0e24b087d
Steve Spicklemire <steve@spvi.com>
2020-09-08 06:36:24 -0400

    move TeXData class

--- genlatex/__init__.py
+++ genlatex/__init__.py
@@ -4,3 +4,4 @@ The `genlatex` module is for creating LaTeX documents from Jijna2 templates.
 
 from .genLatex import main
 from .formatters import latex_float, latex_vec
+from .texData import TeXData
