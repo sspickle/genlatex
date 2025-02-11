621de30ecbfbfb76bb74595e028850c52a0d969a
Steve Spicklemire <steve@spvi.com>
2025-02-11 04:56:13 -0500

    mv version to pyproject.toml

--- src/genlatex/__init__.py
+++ src/genlatex/__init__.py
@@ -2,8 +2,6 @@
 The `genlatex` module is for creating LaTeX documents from Jijna2 templates.
 """
 
-version = '0.28'
-
 from .genLatex import main
 from .formatters import latex_float, latex_vec, eunits
 from .texData import TeXData
